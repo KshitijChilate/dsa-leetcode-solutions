e6a070f44ee0299fdd23c22e8cb6368d63fcfa81
Kshitij Chilate <chilatekshitij11@gmail.com>
2026-03-19 20:47:05 +0530

    Time: 38 ms (51.31%) | Memory: 55.2 MB (30.78%) - LeetSync

---
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int equalPairs(int[][] grid) {
+        int n = grid.length;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++){
+            StringBuilder row = new StringBuilder();
+            for (int j = 0; j < n; j++){
+                row.append(grid[i][j]).append(",");
+            }
+            String key = row.toString();
+            map.put(key, map.getOrDefault(key, 0) + 1);
+        }
+
+        int count = 0;
+
+        for (int j = 0; j < n; j++) {
+            StringBuilder col = new StringBuilder();
+            for (int i = 0; i < n; i++) {
+                col.append(grid[i][j]).append(",");
+            }
+            String key = col.toString();
+
+            count += map.getOrDefault(key, 0);
+        }
+        return count;
+    }
+}
