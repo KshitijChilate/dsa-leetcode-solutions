20f6aff22c02f1dad16354e1d7445d93d4d9e04f
Kshitij Chilate <chilatekshitij11@gmail.com>
2026-04-08 23:45:06 +0530

    Time: 2 ms (95.53%) | Memory: 45.3 MB (98.30%) - LeetSync

---
+++ 39-combination-sum/combination-sum.java
@@ -0,0 +1,27 @@
+class Solution { 
+    static void combinationsFinder(int[] arr,int idx,int target,List<Integer> 
+list,List<List<Integer>> ans){ 
+        if(idx == arr.length){ 
+            if(target==0){ 
+                ans.add(new ArrayList<>(list)); 
+            } 
+            return; 
+              
+        } 
+        if(arr[idx]<=target){ 
+            list.add(arr[idx]); 
+            combinationsFinder(arr,idx,target - arr[idx],list,ans); 
+            list.remove(list.size() - 1); 
+        } 
+        combinationsFinder(arr,idx+1,target,list,ans); 
+    } 
+ 
+    public List<List<Integer>> combinationSum(int[] candidates, int target) { 
+        List<List<Integer>> ans = new ArrayList<>(); 
+         
+        combinationsFinder(candidates,0,target,new ArrayList<>(),ans); 
+         
+        return ans; 
+    } 
+} 
+ 
