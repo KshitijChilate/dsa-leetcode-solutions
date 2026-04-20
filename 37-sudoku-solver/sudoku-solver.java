9fe042139e3d70de6de5edac6ef06a88cdbe25a1
Kshitij Chilate <chilatekshitij11@gmail.com>
2026-04-20 19:58:18 +0530

    Time: 216 ms (62.90%) | Memory: 42.8 MB (55.95%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,39 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        backtrack(board);
+    }
+
+    private boolean backtrack(char[][] board) {
+        for (int r = 0; r < 9; r++) {
+            for (int c = 0; c < 9; c++) {
+                if (board[r][c] == '.') {
+                    for (char ch = '1'; ch <= '9'; ch++) {
+                        if (isValid(board, r, c, ch)) {
+                            board[r][c] = ch;
+                            if (backtrack(board)) return true;
+                            board[r][c] = '.'; // undo
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch) return false;
+            if (board[i][col] == ch) return false;
+        }
+
+        int startRow = 3 * (row / 3);
+        int startCol = 3 * (col / 3);
+        for (int i = startRow; i < startRow + 3; i++) {
+            for (int j = startCol; j < startCol + 3; j++) {
+                if (board[i][j] == ch) return false;
+            }
+        }
+        return true;
+    }
+}
